e94429a841e182d2c68e4d41ca4da8386f450064
rushjun23 <163828892+rushjun23@users.noreply.github.com>
2024-03-18 16:18:45 +0800

    Update alidns.sh
    增加对 .com.cn|.gov.cn|.net.cn|.org.cn|.ac.cn|.gd.cn 域名判断

--- alidns.sh
+++ alidns.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+FLAG=".com.cn|.gov.cn|.net.cn|.org.cn|.ac.cn|.gd.cn"  
+
 
 if ! command -v aliyun >/dev/null; then
 	echo "错误: 你需要先安装 aliyun 命令行工具 https://help.aliyun.com/document_detail/121541.html。" 1>&2
@@ -7,6 +9,14 @@ fi
 
 DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
 SUB_DOMAIN=$(expr match "$CERTBOT_DOMAIN" '\(.*\)\..*\..*')
+
+if echo $CERTBOT_DOMAIN |egrep -q "$FLAG"; then
+
+  DOMAIN=`echo $CERTBOT_DOMAIN |grep -oP '(?<=)[^.]+('$FLAG')'`
+  SUB_DOMAIN=`echo $CERTBOT_DOMAIN |grep -oP '.*(?=\.[^.]+('$FLAG'))'`
+
+fi
+
 if [ -z $DOMAIN ]; then
     DOMAIN=$CERTBOT_DOMAIN
 fi
